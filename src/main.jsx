0bbe5ec5fb05f256bdadafd552023480abae2606
NZTAI <nzt48ai@gmail.com>
2026-03-27 00:23:03 -0400

    Add URL-triggered runtime debug capture overlay

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import DebugRuntimeCapture, { isDebugModeEnabled } from "./debugRuntime";
 import "./index.css";
+
+const debugModeEnabled = isDebugModeEnabled();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <DebugRuntimeCapture enabled={debugModeEnabled}>
+      <App />
+    </DebugRuntimeCapture>
   </React.StrictMode>
 );
